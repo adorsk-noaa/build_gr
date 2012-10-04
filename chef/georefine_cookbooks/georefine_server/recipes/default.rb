51041c5b7bc8943064aabde53ae6a15d0cff9705
adorsk <adorsk>
2012-10-04 17:03:04 -0400

    added apache vhost config

--- chef/georefine_cookbooks/georefine_server/recipes/default.rb
+++ chef/georefine_cookbooks/georefine_server/recipes/default.rb
@@ -9,16 +9,15 @@ user georefine_user do
 end
 
 # Create folder for code, data, and public files.
-['lib', 'data', 'public'].each do |dir_|
+['lib', 'data', 'public', 'wsgi'].each do |dir_|
     dir_path = "#{georefine_dir}/#{dir_}"
     directory dir_path do
         owner georefine_user
+        group georefine_user
     end
 end
 
-# Setup DB.
-georefine_db_user = 'georefine'
-georefine_db_password = 'georefine' #@TODO: read from node attributes or dbag.
+# DB connection.
 georefine_db_connection = {
     :host => "localhost",
     :port => 5432,
@@ -26,12 +25,16 @@ georefine_db_connection = {
     :password => node['postgresql']['password']['postgres']
 }
 
+# Setup DB user.
+georefine_db_user = 'georefine'
+georefine_db_password = 'georefine' #@TODO: read from node attributes or dbag.
 postgresql_database_user georefine_db_user do
     connection georefine_db_connection
     password georefine_db_password
     action :create
 end
 
+# Spatialize db (will be trigger after db is created.)
 postgis_contrib_dir = "/usr/share/postgresql/#{node['postgresql']['version']}/contrib/postgis-1.5"
 postgis_sql = "#{postgis_contrib_dir}/postgis.sql"
 postgis_srs_sql = "#{postgis_contrib_dir}/spatial_ref_sys.sql"
@@ -41,6 +44,7 @@ execute 'spatialize db' do
     action :nothing
 end
 
+# Create db.
 postgresql_database 'georefine' do
     connection georefine_db_connection
     owner georefine_db_user
@@ -48,10 +52,14 @@ postgresql_database 'georefine' do
     notifies :run, resources(:execute => 'spatialize db')
 end
 
+# Setup apache config.
+web_app "georefine" do
+    template "georefine.conf.erb"
+    server_name "georefine.local"
+    server_aliases [node['fqdn']]
+    docroot "#{georefine_dir}/public"
+    app_path "/georefine"
+    app_wsgi_script_dir "#{georefine_dir}/wsgi"
+end
 
 # Setup app config.
-
-# Setup apache config.
-
-
-# Setup virtualenv.
